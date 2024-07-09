fc69a4677aab8cb9c2ac44ebeeb382b81229ae2c
KalpViradia <160616083+KalpViradia@users.noreply.github.com>
2024-07-09 16:07:16 +0530

    Add files via upload

---
+++ Lab4.sql
@@ -0,0 +1,45 @@
+--Alter Operation
+--Part–A :-
+--Use Deposit table of lab-1.
+--From the above given tables perform the following queries (ALTER Operation):
+--1. Add two more columns City VARCHAR (20) and Pincode INT.
+--2. Change the size of CNAME column from VARCHAR (50) to VARCHAR (35).
+--3. Change the data type DECIMAL to INT in amount Column.
+--4. Rename Column ActNo to ANO.
+--5. Delete Column City from the DEPOSIT table.
+--6. Change name of table DEPOSIT to DEPOSIT_DETAIL.
+--Part–B :-
+--1. Rename Column ADATE to AOPENDATE OF DEPOSIT_DETAIL table.
+--2. Delete Column AOPENDATE from the DEPOSIT_DETAIL table.
+--3. Rename Column CNAME to CustomerName.
+--Part–C :-
+--Create following table using query according to the definition.
+--From the above given tables perform the following queries (ALTER Operation):
+--1. Add two more columns City VARCHAR (20) (Not null) and Backlog INT (Null).
+--2. Change the size of NAME column of student_detail from VARCHAR (25) to VARCHAR (35).
+--3. Change the data type DECIMAL to INT in CPI Column.
+--4. Rename Column Enrollment_No to ENO.
+--5. Delete Column City from the student_detail table.
+--6. Change name of table student_detail to STUDENT_MASTER.
+--DELETE, Truncate, Drop Operation
+--Part–A :-
+--Use Deposit_Detail table (Altered table of DEPOSIT)
+--1. Delete all the records of DEPOSIT_DETAIL table having amount greater than and equals to 4000.
+--2. Delete all the accounts CHANDI BRANCH.
+--3. Delete all the accounts having account number (ANO) is greater than 105.
+--4. Delete all the records of Deposit_Detail table. (Use Truncate)
+--5. Remove Deposit_Detail table. (Use Drop)
+--Part–B :-
+--Create following table using query according to the definition.
+--Insert the following records in the EMPLOYEE_MASTER table
+--From the above given tables perform the following queries (DELETE Operation):
+--1. Delete all the records of Employee_MASTER table having salary greater than and equals to 14000.
+--2. Delete all the Employees who belongs to ‘RAJKOT’ city.
+--3. Delete all the Employees who joined after 1-1-2007.
+--4. Delete the records of Employees whose joining date is null and Name is not null.
+--5. Delete the records of Employees whose salary is 50% of 20000.
+--6. Delete the records of Employees whose City Name is not empty.
+--7. Delete all the records of Employee_MASTER table. (Use Truncate)
+--8. Remove Employee_MASTER table. (Use Drop)
+--Part–C :-
+--1. Summarize Delete, Truncate and Drop
