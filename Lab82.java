cca03fb2a4577627cf56b8ff04e8ddbadc196f11
KalpViradia <160616083+KalpViradia@users.noreply.github.com>
2024-09-19 08:24:34 +0530

    Update Lab82.java

--- Lab82.java
+++ Lab82.java
@@ -1,97 +1,83 @@
-import java.util.HashMap;
 import java.util.Scanner;
 
-class treeNode {
-    int val;
-    treeNode left, right;
+class TreeNode1 {
+    int value;
+    TreeNode1 left;
+    TreeNode1 right;
 
-    public treeNode(int val) {
-        this.val = val;
-        this.left = this.right = null;
+    TreeNode1(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
 }
 
-class BinaryTreeConstruction {
-    int preorderIndex;
-    HashMap<Integer, Integer> postorderMap;
+class Lab82 {
 
-    public treeNode constructFromPrePost(int[] preorder, int[] postorder) {
-        preorderIndex = 0;
-        postorderMap = new HashMap<>();
-
-        for (int i = 0; i < postorder.length; i++) {
-            postorderMap.put(postorder[i], i);
-        }
-
-        return constructTree(preorder, postorder, 0, postorder.length - 1);
+    public static TreeNode1 constructTree(int[] preorder, int[] postorder) {
+        return constructTree(preorder, postorder, 0, preorder.length - 1, 0, postorder.length - 1);
     }
 
-    public treeNode constructTree(int[] preorder, int[] postorder, int postStart, int postEnd) {
-        if (postStart > postEnd || preorderIndex >= preorder.length) {
+    private static TreeNode1 constructTree(int[] preorder, int[] postorder, int preStart, int preEnd, int postStart, int postEnd) {
+        if (preStart > preEnd || postStart > postEnd) {
             return null;
         }
 
-        treeNode root = new treeNode(preorder[preorderIndex++]);
-
-        if (postStart == postEnd || preorderIndex >= preorder.length) {
+        TreeNode1 root = new TreeNode1(preorder[preStart]);
+        
+        if (preStart == preEnd) {
             return root;
         }
 
-        int postIndex = postorderMap.get(preorder[preorderIndex]);
+        int leftRootValue = preorder[preStart + 1];
+        int leftRootIndexInPost = findIndex(postorder, leftRootValue, postStart, postEnd);
+        int leftSubtreeSize = leftRootIndexInPost - postStart + 1;
 
-        if (postIndex <= postEnd) {
-            root.left = constructTree(preorder, postorder, postStart, postIndex);
-            root.right = constructTree(preorder, postorder, postIndex + 1, postEnd - 1);
-        }
+        root.left = constructTree(preorder, postorder, preStart + 1, preStart + leftSubtreeSize, postStart, leftRootIndexInPost);
+        root.right = constructTree(preorder, postorder, preStart + leftSubtreeSize + 1, preEnd, leftRootIndexInPost + 1, postEnd - 1);
 
         return root;
     }
 
-    public void printInorder(treeNode node) {
-        if (node == null) return;
-        printInorder(node.left);
-        System.out.print(node.val + " ");
-        printInorder(node.right);
+    private static int findIndex(int[] arr, int value, int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (arr[i] == value) {
+                return i;
+            }
+        }
+        return -1;
     }
-}
 
-public class Lab82 {
+    public static void inorder(TreeNode1 root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.value + " ");
+            inorder(root.right);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the length of the tree: ");
+
+        System.out.print("Enter the number of nodes in the tree: ");
         int n = sc.nextInt();
+
         int[] preorder = new int[n];
         int[] postorder = new int[n];
-        int count = 0;
-        System.out.println("Enter array elements: ");
-        for (int i = 0;i < n;i++) {
-            System.out.println("Enter value for element in index "+i+" for preorder: ");
+
+        System.out.println("Enter the preorder traversal:");
+        for (int i = 0; i < n; i++) {
             preorder[i] = sc.nextInt();
         }
 
-        for (int i = 0;i < n;i++) {
-            System.out.println("Enter value for element in index "+i+" for postorder: ");
+        System.out.println("Enter the postorder traversal:");
+        for (int i = 0; i < n; i++) {
             postorder[i] = sc.nextInt();
         }
 
-        for (int i = 0;i < n; i++) {
-            for (int j = 0;j < n; j++) {
-                if (postorder[j] == preorder[i]) {
-                    count++;
-                }
-            }   
-        }
-
-        if (count == n) {
-            BinaryTreeConstruction tree = new BinaryTreeConstruction();
-            treeNode root = tree.constructFromPrePost(preorder, postorder);
-
-            System.out.println("Inorder traversal of the constructed tree:");
-            tree.printInorder(root);
-        }
-        else {
-            System.out.println("Not a valid binary tree");
-        }
+        TreeNode1 root = constructTree(preorder, postorder);
+        System.out.println("Inorder traversal of constructed tree:");
+        inorder(root);
 
         sc.close();
     }
